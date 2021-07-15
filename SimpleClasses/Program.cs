86fdd691fc22418580066446df3ed5619b03c9aa
Евгений <rabota142@inbox.ru>
2021-07-15 23:17:01 +0300

    Added some functions

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -9,20 +9,41 @@ namespace SimpleClasses
             vector<int> vec = new vector<int>(1,2,3);
             Console.WriteLine("Size = " + vec.get_size());
             Console.WriteLine("Capcaity = " + vec.get_capacity());
-            vec.printVector();
+            vec.printVector("\n");
+            Console.WriteLine("\n___________________");
             vector<int> vec_2 = new vector<int>(4,5,6,7);
             Console.WriteLine("Size = " + vec_2.get_size());
             Console.WriteLine("Capcaity = " + vec_2.get_capacity());
-            vec_2.printVector();
+            vec_2.printVector(", ");
+            Console.WriteLine("\n____________________");
             vector<int> vec_3 = vec + vec_2;
             Console.WriteLine("Size = " + vec_3.get_size());
             Console.WriteLine("Capcaity = " + vec_3.get_capacity());
-            //vec_3.printVector();
             vec_3[0] = 10;
             for (int i = 0; i < vec_3.get_size(); ++i)
             {
                 Console.Write(vec_3[i] + " ");
             }
+            Console.WriteLine("\n____________________");
+            Console.WriteLine("Pop back");
+            vec_3.pop_back();
+            Console.WriteLine("Size = " + vec_3.get_size());
+            Console.WriteLine("Capcaity = " + vec_3.get_capacity());
+            vec_3.reserve(176);
+            Console.WriteLine("\n____________________");
+            Console.WriteLine("Reserve");
+            Console.WriteLine("Size = " + vec_3.get_size());
+            Console.WriteLine("Capcaity = " + vec_3.get_capacity());
+            Console.WriteLine("\n____________________");
+            vec_3.resize(3);
+            Console.WriteLine("Size = " + vec_3.get_size());
+            Console.WriteLine("Capcaity = " + vec_3.get_capacity());
+            vec_3.printVector();
+            Console.WriteLine("\n____________________");
+            vec_3.resize(30);
+            Console.WriteLine("Size = " + vec_3.get_size());
+            Console.WriteLine("Capcaity = " + vec_3.get_capacity());
+            vec_3.printVector();
         }
     }
 }
--- SimpleClasses/Vector.cs
+++ SimpleClasses/Vector.cs
@@ -14,14 +14,12 @@ namespace SimpleClasses
             capacity = 0;
             my_data = new T[capacity];
         }
-
         public vector(int size)
         {
             this.size = size;
             capacity = size * 2;
             my_data = new T[capacity];
         }
-
         public vector(params T[] args)
         {
             size = args.Length;
@@ -32,13 +30,36 @@ namespace SimpleClasses
                 my_data[i] = args[i];
             }
         }
-
+        ~vector()
+        {
+            clear();
+        }
+        public T this[int key]
+        {
+            get => my_data[key];
+            set => my_data[key] = value;
+        }
+        public static vector<T> operator+ (vector<T> vec, vector<T> other)
+        {
+            T[] temp_data = new T[vec.size + other.size];
+            for (int i = 0; i < vec.size; ++i)
+            {
+                temp_data[i] = vec.my_data[i];
+            }
+            for (int i = vec.size; i < other.size + vec.size; ++i)
+            {
+                temp_data[i] = other.my_data[i - vec.size];
+            }
+            vector<T> result = new(temp_data);
+            return result;
+        }
         public void push_back(T value)
         {
             if (size == capacity)
             {
                 capacity *= 2;
                 T[] temp_data = my_data;
+                my_data = null;
                 my_data = new T[capacity];
                 for (int i = 0; i < size; ++i)
                 {
@@ -52,36 +73,71 @@ namespace SimpleClasses
             }
             size++;
         }
-
-        public T this[int key]
+        public void clear()
         {
-            get => my_data[key];
-            set => my_data[key] = value;
+            my_data = null;
+            size = 0;
+            capacity = 0;
         }
-
-        public static vector<T> operator+ (vector<T> vec, vector<T> other)
+        public bool empty() { return size == 0; }
+        public void resize(int count)
         {
-            T[] temp_data = new T[vec.size + other.size];
-            for (int i = 0; i < vec.size; ++i)
+            if (size > count)
             {
-                temp_data[i] = vec.my_data[i];
+                capacity = count * 2;
+                size = count;
+                T[] temp_data = my_data;
+                my_data = null;
+                my_data = new T[capacity];
+                for (int i = 0; i < size; ++i)
+                {
+                    my_data[i] = temp_data[i];
+                }
             }
-            for (int i = vec.size; i < other.size + vec.size; ++i)
+            else if (size < count)
             {
-                temp_data[i] = other.my_data[i - vec.size];
+                capacity = count * 2;
+                T[] temp_data = my_data;
+                my_data = null;
+                my_data = new T[capacity];
+                for (int i = 0; i < size; ++i)
+                {
+                    my_data[i] = temp_data[i];
+                }
+                size = count;
             }
-            vector<T> result = new vector<T>(temp_data);
-            return result;
         }
-
-        public void printVector()
+        public void reserve(int capacity)
+        {
+            if (this.capacity < capacity)
+            {
+                this.capacity = capacity;
+                T[] temp_data = my_data;
+                my_data = null;
+                my_data = new T[capacity];
+                for (int i = 0; i < size; ++i)
+                {
+                    my_data[i] = temp_data[i];
+                }
+            }
+        }
+        public void pop_back()
+        {
+            // So bad
+            if (size > 1)
+            {
+                my_data[size - 1] = my_data[size];
+                --size;
+            }
+        }
+        public void printVector(string control = " ")
         {
             for (int i = 0; i < size; ++i)
             {
-                Console.WriteLine(my_data[i] + " ");
+                if (i + 1 != size) Console.Write(my_data[i] + $"{control}");
+                else Console.Write(my_data[i]);
             }
         }
-
         public int get_size() { return size; }
         public int get_capacity() { return capacity; }
 
