7dd9f30bccbd5715d1bfddc4f8bd1b66ca03dd57
Евгений <rabota142@inbox.ru>
2021-07-15 22:03:30 +0300

    First commit

--- SimpleClasses/Program.cs
+++ SimpleClasses/Program.cs
@@ -2,11 +2,59 @@
 
 namespace SimpleClasses
 {
+    public class vector<T>
+    {
+        public vector()
+        {
+            size = 0;
+            capacity = 0;
+            my_data = new T[capacity];
+        }
+
+        public vector(int size)
+        {
+            this.size = size;
+            this.capacity = size * 2;
+            my_data = new T[capacity];
+        }
+
+        public vector(params T[] args)
+        {
+            this.size = args.Length;
+            this.capacity = this.size * 2;
+            my_data = new T[capacity];
+            for (int i = 0; i < size; ++i)
+            {
+                my_data[i] = args[i];
+            }
+        }
+
+        public void printVector()
+        {
+            for (int i = 0; i < size; ++i)
+            {
+                Console.WriteLine(my_data[i] + " ");
+            }
+        }
+
+        public int get_size() { return this.size; }
+        public int get_capacity() { return this.capacity; }
+
+        private T[] my_data;
+        private int size;
+        private int capacity;
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            vector<int> vec;
+            vec = new vector<int>(1,2,3);
+            Console.WriteLine(vec.get_size());
+            Console.WriteLine(vec.get_capacity());
+            vec.printVector();
         }
     }
 }
